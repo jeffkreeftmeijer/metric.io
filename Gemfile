8fbed3dda9cc33d1fa1ab5b064ae432049f4cc5d
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2011-03-14 13:38:31 +0100

    Vanilla Rails Edge app

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+
+gem 'rails', :git => 'git://github.com/rails/rails.git'
+gem "rack", :git => "git://github.com/rack/rack.git"
+gem 'sqlite3'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetricIo::Application.config.secret_token = '2b2b57f403fc51dc7554e8acd2686309440f3317fe43a56aeebd3058c72a394a83d3b322cf9fd094b79e5bd19ff6ef2910641f89a06db6ae5c13c930a5f8fb6f'
